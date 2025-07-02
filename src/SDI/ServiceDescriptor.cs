4e00b0e6997e44ffc8e75deddbb870799d37b3e4
WrongSell <84908244+R2R2HS@users.noreply.github.com>
2025-07-02 22:43:02 +0300

    Added ServiceDescriptor Factory Methods

--- src/SDI/ServiceDescriptor.cs
+++ src/SDI/ServiceDescriptor.cs
@@ -12,4 +12,10 @@ public class ServiceDescriptor(Type serviceType, Type implementationType, Type l
     public Type LifeTimeType => lifeTimeType;
 
     public object Key => key;
+
+    public static IServiceDescriptor Create<TService, TImplementation, TLifeTime>() where TImplementation : TService where TLifeTime : IServiceLifeTime => 
+        Create<TService, TImplementation, TLifeTime>(null);
+
+    public static IServiceDescriptor Create<TService, TImplementation, TLifeTime>(object key) where TImplementation : TService where TLifeTime : IServiceLifeTime => 
+        new ServiceDescriptor(typeof(TService), typeof(TImplementation), typeof(TLifeTime), key);
 }
